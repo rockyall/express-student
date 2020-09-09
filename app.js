49a1fb6df022c10422405010e7ececa730d51583
RickyWrong <48841112+RickyWrong@users.noreply.github.com>
2020-09-09 00:36:29 -0600

    Nuevos cambios para el backend server

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const assert = require("assert");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const unitec = require("./routes/unitec");
+const user = require("./routes/user");
 const dev = require("./routes/dev");
 const port = process.env.PORT || 3006;
 console.clear();
@@ -21,6 +22,7 @@ app.use(welcome);
 
 //Routers
 app.use("/api/unitec", unitec);
+app.use("/api/user", user);
 app.use("/api/dev", dev);
 
 //Via de acceso a raiz
